8155c88988d0cf6bae503a7e44bb30bb92b1368a
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-09-21 12:21:53 +0800

    category

--- resources/js/Pages/Category.vue
+++ resources/js/Pages/Category.vue
@@ -1,3 +1,9 @@
+<script setup>
+    import BreezeAuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+    import PrimaryButton from '@/components/PrimaryButton.vue'
+</script>
 <template>
-    Hello Vue. This is me
+    <BreezeAuthenticatedLayout>
+        Hello Vue. This is me <PrimaryButton type="button">Test</PrimaryButton>
+    </BreezeAuthenticatedLayout>
 </template>
